6377f0aa8aecd7a98480e65118d95a0c30085659
taherul77 <fmnazmuss@gmail.com>
2023-05-04 17:25:53 +0600

    add recipe data

--- index.js
+++ index.js
@@ -6,12 +6,26 @@ const cors = require('cors')
 app.use(cors())
 
 const chef = require("./data/chef.json")
+const recipes = require("./data/recipe.json")
 
 app.get('/', (req, res) => {
   res.send('Perfect Food!')
 })
 app.get("/chef", (req, res) => {
   res.send(chef);
+});
+
+
+
+
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const singleChef = chef.find((c) => c.id === id);
+  res.send( singleChef);
+});
+
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
 });
 
 app.listen(port, () => {
